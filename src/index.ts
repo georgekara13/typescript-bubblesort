33a820e4d4663ef09e133c3c0955068541439a70
georgekara13 <george_kara_13@hotmail.com>
2021-10-23 16:09:40 +0300

    Abstract sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 33, -5, 0]);
-const charactersCollection = new CharactersCollection("ghXkawIf");
-const sorter = new Sorter(numbersCollection);
-const charSorter = new Sorter(charactersCollection);
-
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
-charSorter.sort();
+const charactersCollection = new CharactersCollection("XuauehG");
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(122);
+linkedList.add(-10);
+linkedList.add(-20);
+linkedList.add(0);
+
+linkedList.sort();
+linkedList.print();
